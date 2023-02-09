52b90c9596ec1807905e7a0d68e9072c5694cecc
Anuj Kaushik <anujkaushik1512@gmail.com>
2023-02-09 21:11:12 +0530

    changed dark to light theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
 import '@/styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const customTheme = extendTheme({
+  colors: {
+    brand: {
+      900: '#fff',
+      800: '#f2f2f2',
+      700: '#d9d9d9',
+    },
+  },
+})
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+
+  )
 }
---
+++ src/pages/signup/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+
+function index() {
+  return (
+    <div className='signup-container'>
+
+      <div className='signup-card'>
+
+        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+
+      </div>
+
+    </div>
+  )
+}
+
+export default index
