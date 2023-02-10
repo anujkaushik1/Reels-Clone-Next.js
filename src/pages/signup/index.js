7991b0f00e23f1c9e45a5d5684f634cea002e416
Anuj Kaushik <anujkaushik1512@gmail.com>
2023-02-10 17:38:56 +0530

    Implemented Designs for signup page

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,15 +1,53 @@
 import React from 'react'
 import TextField from '@mui/material/TextField';
+import classes from './signup.module.css';
+import Image from 'next/image';
+import instaImage from '../../../assets/images.png';
+import Button from '@mui/material/Button';
 
 function index() {
   return (
-    <div className='signup-container'>
-        <div className='signup-card'>
-    
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    <div className={classes.main}>
+      <div className={classes.signupContainer}>
+        <div className={classes.signupCard}>
+
+          <Image
+            style={{
+              height: '25%',
+              width: '80%'
+            }}
+            src={instaImage} />
+          <TextField
+            id="outlined-basic" label="Email" variant="outlined" fullWidth
+            size='small' type='email' margin='dense' />
+
+          <TextField
+            id="outlined-basic" label="Password" variant="outlined" fullWidth
+            size='small' type='password' margin='dense' />
+
+          <TextField
+            id="outlined-basic" label="Full Name" variant="outlined" fullWidth
+            size='small' type='text' margin='dense' />
+
+
+          <Button fullWidth sx={{marginTop : '20px'}} variant="outlined" component="label">
+            Upload
+            <input hidden accept="image/*" multiple type="file" />
+          </Button>
+
+          <Button fullWidth sx={{marginTop : '20px'}} variant="contained">
+            Sign up
+          </Button>
 
         </div>
 
+        <div className={classes.bottomCard}>
+              Already have an account? <span style={{
+                color : 'blue'
+              }}>Login</span>
+        </div>
+
+      </div>
     </div>
   )
 }
